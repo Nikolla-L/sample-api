736df0324113164b4699068bbf7f1a450b07f81e
Nikolla-L <nlukava2002@gmail.com>
2022-12-14 02:05:10 +0400

    update controllers

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +15,45 @@ func Verifypassword(userPassword string, givenPassword string) (bool, string) {
 }
 
 func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		vat ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON{http.StatusBadRequest, gin.H{"error": err.Error()}}
+			return 
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bsonM{"error": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H, gin.H{"error": "user alerady exists"})
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if arr != nil {
+			log.Paniv(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone number is taken"})
+			return
+		}
+	}
 }
 
 func Login() gin.HandlerFunc {
--- database/databasetup.go
+++ database/databasetup.go
@@ -37,9 +37,11 @@ func DBSet() *mongo.Client {
 var client *mongodb.Client = DBSet()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
 }
 
 func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var productcollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
 }
